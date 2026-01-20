5b58bf739b629154486ddd845fea2fbc0ecfecdb
ayuubthegreat <yusufayuub00@gmail.com>
2026-01-20 16:32:59 -0600

    git commit -m "Updated website details and improved project page content"

--- BASE_URL.js
+++ BASE_URL.js
@@ -1,7 +1,7 @@
 export const WEBSITE_NAME = "Crescent Moon's Website"
 export const FANDOM_NAME = "Crescent Wiki Fandom"
 export const WEBSITE_ID = 2
-export const BASE_URL = "http://localhost:3001"
+export const BASE_URL = "https://crowdmindapi-fully-online-1.onrender.com/"
 export const STUDIO_NAME = "Crescent Moon Studio"
 export const SUPPORT_EMAIL = "yusufayuub00@gmail.com"
 export const DAYS_UNTIL_RELEASE_WARNING = 7
